d09e5ac189fa6b27bf8a62244b0dfd4146d88a97
Abel <abelmqueralto@gmail.com>
2024-02-10 15:56:30 +0000

    First commit

---
+++ build/CMakeFiles/3.22.1/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.5.0-15-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.5.0-15-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/opt/dey/4.0-r4/ccimx8x-sbc-pro/sysroots/x86_64-deysdk-linux/usr/share/cmake/cortexa35-dey-linux-toolchain.cmake")
+
+set(CMAKE_SYSTEM "Linux")
+set(CMAKE_SYSTEM_NAME "Linux")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "aarch64")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ tool/main.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file main.cpp
+ * @author amq84
+ * @brief 
+ * @version 0.1
+ * @date 2024-02-10
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+#include <stdio.h>
+#include "aws/crt/Api.h"
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
